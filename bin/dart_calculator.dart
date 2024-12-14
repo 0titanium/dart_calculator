1adad61059beac268cd68196c0d7a4d0b611a88a
kimhyungmin <jeen14rla@gmail.com>
2024-12-14 18:42:33 +0900

    feat: dart_calculator 작성

--- bin/dart_calculator.dart
+++ bin/dart_calculator.dart
@@ -1,5 +1,28 @@
-import 'package:dart_calculator/calculator.dart' as dart_calculator;
+import 'package:dart_calculator/calculator.dart';
+import 'package:dart_calculator/core/operations/addition_operation.dart';
+import 'package:dart_calculator/core/operations/division_operation.dart';
+import 'package:dart_calculator/core/operations/multiplication_operation.dart';
+import 'package:dart_calculator/core/operations/subtraction_operation.dart';
+import 'package:dart_calculator/input_parser.dart';
 
-void main(List<String> arguments) {
+void main(List<String> args) {
+  final inputParser = InputParser();
+  final parsedInput = inputParser.parseInput(args);
 
+  final operations = {
+    '+': AdditionOperation(),
+    '-': SubtractionOperation(),
+    '*': MultiplicationOperation(),
+    '/': DivisionOperation(),
+  };
+
+  final calculator = Calculator(operations);
+
+  final result = calculator.calculate(
+    parsedInput[0],
+    parsedInput[2],
+    parsedInput[1],
+  );
+
+  print('Result: $result');
 }
