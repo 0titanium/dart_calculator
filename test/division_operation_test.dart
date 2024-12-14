6c17d0dec4e44e475b818c2daec9ea3066cf8dd9
kimhyungmin <jeen14rla@gmail.com>
2024-12-14 14:37:01 +0900

    test: calculator 변수 상단으로 이동

--- test/division_operation_test.dart
+++ test/division_operation_test.dart
@@ -3,18 +3,16 @@ import 'package:test/test.dart';
 import 'package:dart_calculator/calculator.dart';
 
 void main() {
+  final calculator = Calculator();
+
   group('나눗셈 연산 테스트', () {
     test(('나눗셈 연산 4 / 2는 2를 리턴'), () {
-      final calculator = Calculator();
-
       expect(calculator.divisionOperation(4, 2), 2);
     });
 
     test(('나눗셈 연산 2 / 0은 DivisionByZeroError'), () {
-      final calculator = Calculator();
-
       expect(() => calculator.divisionOperation(2, 0),
-          throwsA(isA<DivisionByZeroError>));
+          throwsA(isA<DivisionByZeroError>()));
     });
   });
 }
