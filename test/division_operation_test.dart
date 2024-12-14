18f1c4c7a12a2c88a475258207ee30121e308dd1
kimhyungmin <jeen14rla@gmail.com>
2024-12-14 14:25:26 +0900

    test: 0으로 나누기 에러 테스트 작성

--- test/division_operation_test.dart
+++ test/division_operation_test.dart
@@ -1,10 +1,20 @@
+import 'package:dart_calculator/errors/division_by_zero_error.dart';
 import 'package:test/test.dart';
 import 'package:dart_calculator/calculator.dart';
 
 void main() {
-  test(('나눗셈 연산 4 / 2는 2를 리턴'), () {
-    final calculator = Calculator();
+  group('나눗셈 연산 테스트', () {
+    test(('나눗셈 연산 4 / 2는 2를 리턴'), () {
+      final calculator = Calculator();
 
-    expect(calculator.divisionOperation(4, 2), 2);
+      expect(calculator.divisionOperation(4, 2), 2);
+    });
+
+    test(('나눗셈 연산 2 / 0은 DivisionByZeroError'), () {
+      final calculator = Calculator();
+
+      expect(() => calculator.divisionOperation(2, 0),
+          throwsA(isA<DivisionByZeroError>));
+    });
   });
 }
