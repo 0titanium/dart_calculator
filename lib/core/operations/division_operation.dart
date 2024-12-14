951544f562524074135c4517a303cdaf60954589
kimhyungmin <jeen14rla@gmail.com>
2024-12-14 17:51:12 +0900

    refactor: exception 적용

--- lib/core/operations/division_operation.dart
+++ lib/core/operations/division_operation.dart
@@ -1,11 +1,11 @@
-import 'package:dart_calculator/core/errors/division_by_zero_error.dart';
+import 'package:dart_calculator/core/exceptions/calculator_exceptions.dart';
 import 'package:dart_calculator/core/interfaces/operation.dart';
 
 class DivisionOperation implements Operation {
   @override
   double execute(double firstOperand, double secondOperand) {
     if (secondOperand == 0) {
-      throw DivisionByZeroError();
+      throw DivisionByZeroException('0으로 나눌 수 없습니다.');
     }
 
     return firstOperand / secondOperand;
